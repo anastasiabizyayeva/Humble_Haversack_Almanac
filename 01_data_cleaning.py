dcd34eba0c16802b4b53dd07f035494b62732132
Anastasia Bizyayeva <70596760+anastasiabizyayeva@users.noreply.github.com>
2020-12-21 16:44:54 +0000

    merged dataframes

--- 01_data_cleaning.py
+++ 01_data_cleaning.py
@@ -7,8 +7,12 @@ Created on Mon Dec 21 16:33:56 2020
 import pandas as pd
 import numpy as np 
 
-df = pd.read_csv('original_csvs/EtsySoldOrderItems2020.csv')
+df_one = pd.read_csv('original_csvs/EtsySoldOrderItems2020.csv')
 df_two = pd.read_csv('original_csvs/EtsySoldOrders2020.csv')
 
-print(df.head())
+print(df_one.head())
 print(df_two.head())
+
+df = pd.merge(df_one, df_two, how='outer', on='Order ID', left_index=True, right_index=False)
+
+print(df.head())
