00151eefc3426dc0e1321c61f8b4ac8d4256a5f6
Anastasia Bizyayeva <70596760+anastasiabizyayeva@users.noreply.github.com>
2020-12-22 22:42:56 +0000

    dropped unnecessary columns

--- 01_data_cleaning.py
+++ 01_data_cleaning.py
@@ -10,9 +10,10 @@ import numpy as np
 df_one = pd.read_csv('original_csvs/EtsySoldOrderItems2020.csv')
 df_two = pd.read_csv('original_csvs/EtsySoldOrders2020.csv')
 
-print(df_one.head())
-print(df_two.head())
-
 df = pd.merge(df_one, df_two, how='outer', on='Order ID', left_index=True, right_index=False)
 
-print(df.head())
+print(df.columns)
+
+df_clean= df.drop(['Sale Date_y','Date Posted_y','Delivery City_y','Delivery State_y','Delivery Zipcode_y','Delivery Country_y','Currency_y','Coupon Code_y','Coupon Details_y','Discount Amount_y','Delivery Discount_y','Buyer_y','Order Type_y', 'Payment Type_y', 'Currency_x','Delivery Address1','Delivery Address2','Delivery Zipcode_x','Order Type_x','Listings Type','Payment Type_x','InPerson Discount','InPerson Location', 'VAT Paid by Buyer','SKU','Buyer_x', 'Full Name','Last Name','Payment Method','Status','Adjusted Order Total','Adjusted Card Processing Fees','Adjusted Net Order Amount'], axis=1)
+
+print(df_clean.columns)
